34a2cc75e9cbe4c7dc5e76b16d51324a367376c7
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-05-17 19:59:31 -0300

    Fix Artwork parameter

--- artwork.py
+++ artwork.py
@@ -6,7 +6,7 @@ class Artwork(Embed):
   def __init__(self, title, original_message, made_with, colabURL, imageURL, author):
     description = "Hecho con **{}**\n\
       Pruebalo tú en: [Google Colab]({})\n\n\
-      [Ver mensaje original]({})".format(made_with, made_with, original_message)
+      [Ver mensaje original]({})".format(made_with, colabURL, original_message)
     
     
     super.__init__(title, description, imageURL, author, None, 5570309, None, None)
