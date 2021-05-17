fbe4ef1627cfa91ae1d61f6dfdc07ffc50e0778a
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-05-17 19:55:20 -0300

    Hecho el modelo de mensaje de Webhook

---
+++ webhook_message.py
@@ -0,0 +1,24 @@
+class WebhookMessage():
+
+  webhookURL = None
+  username = None
+  avatarURL = None
+  content = None
+  embeds = []
+  
+  def __init__(self, webhookURL, username, avatarURL, content, embeds):
+    self.webhookURL = webhookURL
+    self.username   = username
+    self.avatarURL  = avatarURL
+    self.content    = content
+    self.embeds     = embeds
+
+
+  def as_dict(self):
+    dictionary = {}
+    dictionary['username']  = self.username
+    dictionary['avatarURL'] = self.avatarURL
+    if self.content:
+      dictionary['content'] = self.content
+    if len(self.embeds):
+      dictionary['embeds']  = self.embeds
