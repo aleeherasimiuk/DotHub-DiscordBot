b61404c8364dc8ea7ee62191fd0a671e28cb6ec3
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-05-17 19:58:30 -0300

    Agregada la descripción default de Artwork

--- artwork.py
+++ artwork.py
@@ -3,5 +3,10 @@ from embed import Embed
 
 class Artwork(Embed):
   
-  def __init__(self, title, description, imageURL, author):
+  def __init__(self, title, original_message, made_with, colabURL, imageURL, author):
+    description = "Hecho con **{}**\n\
+      Pruebalo tú en: [Google Colab]({})\n\n\
+      [Ver mensaje original]({})".format(made_with, made_with, original_message)
+    
+    
     super.__init__(title, description, imageURL, author, None, 5570309, None, None)
