cc678897dec564247b2dfe8aff9a4112186f4dfe
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-05-18 12:32:38 -0300

    Script para enviar mensajes al museo

---
+++ send_artwork.py
@@ -0,0 +1,29 @@
+import requests
+from config import Config
+from artwork import Artwork
+from webhook_message import WebhookMessage
+import json
+import time
+
+config = Config("config.json")
+
+file = open('museo.json')
+museum = json.load(file)['data']
+
+def send(data, title):
+  result = requests.post(config.webhook_url, json = data)
+
+  try:
+      result.raise_for_status()
+  except requests.exceptions.HTTPError as err:
+      print(err)
+  else:
+      print("Se ha añadido {} al museo de Arte.".format(title))
+
+for artwork in museum:
+  art = Artwork(artwork['title'], artwork['original_url'], 'VQGAN + Clip', config.colab_url, artwork['image_url'], artwork['author'])
+  message = WebhookMessage(config.webhook_url, config.username, config.avatar_url, embeds=[art], content= "")
+  send(message.as_dict(), artwork['title'])
+  time.sleep(10)
+
+file.close()
