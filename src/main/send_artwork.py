018d2295c6e81c9a731c77a125fb25fcd4859767
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-05-22 23:22:59 -0300

    La responsabilidad de enviarse es del mensaje

--- src/main/send_artwork.py
+++ src/main/send_artwork.py
@@ -10,20 +10,10 @@ config = Config("../res/config.json")
 file = open('../res/museo.json')
 museum = json.load(file)['data']
 
-def send(data, title):
-  result = requests.post(config.webhook_url, json = data)
-
-  try:
-      result.raise_for_status()
-  except requests.exceptions.HTTPError as err:
-      print(err)
-  else:
-      print("Se ha añadido {} al museo de Arte.".format(title))
-
 for artwork in museum:
   art = Artwork(artwork['title'], artwork['original_url'], 'VQGAN + Clip', config.colab_url, artwork['image_url'], artwork['author'])
   message = WebhookMessage(config, embeds=[art], content= "")
-  send(message.as_dict(), artwork['title'])
+  message.send(message.as_dict())
   time.sleep(10)
 
 file.close()
--- src/main/webhook_message.py
+++ src/main/webhook_message.py
@@ -1,3 +1,4 @@
+import requests
 from src.main.config import Config
 class WebhookMessage():
 
@@ -9,19 +10,29 @@ class WebhookMessage():
   
   def __init__(self, config: Config,  embeds, content = ""):
     self.webhook_url = config.webhook_url
-    self.username   = config.username
+    self.username    = config.username
     self.avatar_url  = config.avatar_url
-    self.content    = content
-    self.embeds     = embeds
+    self.content     = content
+    self.embeds      = embeds
 
 
   def as_dict(self):
     dictionary = {}
-    dictionary['username']  = self.username
+    dictionary['username']   = self.username
     dictionary['avatar_url'] = self.avatar_url
     if self.content:
-      dictionary['content'] = self.content
+      dictionary['content']  = self.content
     if len(self.embeds):
-      dictionary['embeds']  = [embed.as_dict() for embed in self.embeds]
+      dictionary['embeds']   = [embed.as_dict() for embed in self.embeds]
 
     return dictionary
+
+  def send(self):
+    result = requests.post(self.webhook_url, json = self.as_dict())
+
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        print("El mensaje se ha enviado satisfactoriamente")
