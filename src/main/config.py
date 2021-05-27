5180f1d76f1c4a9259a28812c7a07b817c6353af
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-05-26 22:47:43 -0300

    Refactor config with tests and exeptions

--- src/main/config.py
+++ src/main/config.py
@@ -1,33 +1,45 @@
 import json
 
-
 class Config():
 
-  webhook_url = None
-  avatar_url  = None
-  colab_url   = None
-  username    = None
+  webhook_url: str
+  avatar_url : str
+  username   : str
 
-  def __init__(self, webhook_url, avatar_url, username, colab_url):
+  def __init__(self, webhook_url, avatar_url, username):
+    self._validate_webhook_url(webhook_url)
     self.webhook_url = webhook_url
     self.username    = username
     self.avatar_url  = avatar_url
-    self.colab_url   = colab_url
 
   @classmethod
   def from_file(self, config_file):
-    file = open(config_file)
-    __json = json.load(file)
-    file.close()
-    return self.from_dict(__json)
+    __json = self._open_file(config_file)
+    return self.from_json(__json)
 
   @classmethod
-  def from_dict(self, dictionary):
-    webhook_url = dictionary.get('webhook_url')
-    avatar_url  = dictionary.get('avatar_url')
-    username    = dictionary.get('username')
-    colab_url   = dictionary.get('colab')
-    return Config(webhook_url, avatar_url, username, colab_url)
+  def from_json(self, json):
+    try:
+      return Config(**json)
+    except TypeError as err:
+      raise Exception("There was a problem parsing json config. Have you made a mistake? Key failing: {}".format(self._get_bad_keyword(err)))
 
+  @classmethod
+  def _open_file(self, config_file):
+    try:
+      file = open(config_file, "rt")
+      __json = json.load(file)
+      file.close()
+    except FileNotFoundError as err:
+      raise Exception("There was a problem reading config file: {}. Please try again and make sure the file exists. [{}]".format(config_file, err.args))
 
+    return __json
   
+  @classmethod
+  def _get_bad_keyword(self, error):
+    return error.args[0].split("'")[1]
+
+
+  def _validate_webhook_url(self, webhook_url):
+    if not webhook_url.startswith("https://discord.com/api/webhooks/"):
+      raise Exception("The webhook_url is not valid.")
---
+++ test/config_test.py
@@ -0,0 +1,75 @@
+import unittest
+from src.main.config import Config
+
+
+class ConfigTest(unittest.TestCase):
+  
+  def test_config_file_does_not_exist(self):
+    with self.assertRaises(Exception):
+      Config.from_file("this_file_does_not_exist")
+
+  def test_error_on_config_key_typo(self):
+    fake_json = {
+      'username': 'username',
+      'webhook_url': 'webhook_url',
+      'avatar-url': 'avatar_url'
+    }
+    with self.assertRaises(Exception) as error:
+      Config.from_json(fake_json)
+      
+  def test_error_on_config_key_key_invalid(self):
+    fake_json = {
+      'username': 'username',
+      'webhook_url': 'webhook_url',
+      'avatar_url': 'avatar_url',
+      'fake_key': 'fake_key'
+    }
+    with self.assertRaises(Exception) as error:
+      Config.from_json(fake_json)
+    
+  def test_error_on_config_key_key_missing(self):
+    fake_json = {
+      'username': 'username',
+      'webhook_url': 'webhook_url',
+    }
+    with self.assertRaises(Exception) as error:
+      Config.from_json(fake_json)
+
+
+  def test_valid_config_username(self):
+    valid_json = {
+      'username': 'username',
+      'webhook_url': 'https://discord.com/api/webhooks',
+      'avatar_url': 'avatar_url'
+    }
+    config = Config.from_json(valid_json)
+    self.assertEquals('username', config.username)
+
+  def test_valid_config_webhook_url(self):
+    valid_json = {
+      'username': 'username',
+      'webhook_url': 'https://discord.com/api/webhooks',
+      'avatar_url': 'avatar_url'
+    }
+    config = Config.from_json(valid_json)
+    self.assertEquals('https://discord.com/api/webhooks', config.webhook_url)
+
+  def test_valid_config_avatar(self):
+    valid_json = {
+      'username': 'username',
+      'webhook_url': 'https://discord.com/api/webhooks',
+      'avatar_url': 'avatar_url'
+    }
+    config = Config.from_json(valid_json)
+    self.assertEquals('avatar_url', config.avatar_url)
+
+  def test_invalid_config_webhook_url(self):
+    valid_json = {
+      'username': 'username',
+      'webhook_url': 'https://google.com',
+      'avatar_url': 'avatar_url'
+    }
+    with self.assertRaises(Exception):
+      Config.from_json(valid_json)
+
+  
