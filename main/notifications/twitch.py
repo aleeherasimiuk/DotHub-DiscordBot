139e2bbc4d19af689177e9d5a5513dcdae8aa5a2
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-06-01 00:10:48 -0300

    Bold twitch stream title

--- main/notifications/twitch.py
+++ main/notifications/twitch.py
@@ -5,5 +5,5 @@ class Twitch(WebhookMessage):
 
   def __init__(self, config: Config, user_name, user_login, title):
 
-    content = "Hey! [@{}](https://twitch.tv/{}) está en directo en Twitch: {}.\nNo te lo pierdas!".format(user_name, user_login, title)
+    content = "Hey! [@{}](https://twitch.tv/{}) está en directo en Twitch: **{}**.\nNo te lo pierdas!".format(user_name, user_login, title)
     super().__init__(config, [], content=content)
