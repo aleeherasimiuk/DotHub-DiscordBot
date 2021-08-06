0f9a5d3e525c3c1f5d4e6337945732213a8e7607
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-08-06 18:14:16 -0300

    Added json for token and changing names

--- moderadot.py
+++ moderadot.py
@@ -2,32 +2,40 @@ import discord
 from discord.ext import commands
 from os import system
 from datetime import datetime
+import json
 
-mensaje = ""
-lmensaje = ""
-n = 0
+CONFIG_FILE = "res/moderadot.json"
+
+bot_config = None
+with open(CONFIG_FILE) as file:
+    bot_config = json.load(file)
+
+message_content = ""
+last_message = ""
+spam_counter = 0
 
 bot = commands.Bot(command_prefix='>')
-@bot.listen()
+
+@bot.event
 async def on_message(message):
-    global mensaje, lmensaje, n
+    global message_content, last_message, spam_counter
     date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    mensaje = f"{message.author.id}: {message.content}"
-    mensaje = mensaje.replace("'", "<c>")
-    if mensaje == lmensaje:
-        n = n+1
+    message_content = f"{message.author.id}: {message.content}"
+    message_content = message_content.replace("'", "<c>")
+    if message_content == last_message:
+        spam_counter += 1
         await message.delete()
-        system(f"echo '{date} {mensaje}' >> muted.log")
+        system(f"echo '{date} {message_content}' >> muted.log")
     else:
-        lmensaje = mensaje
-        n = 0
-        system(f"echo '{date} {mensaje}' >> all.log")
-    if n > 1:
+        last_message = message_content
+        spam_counter = 0
+        system(f"echo '{date} {message_content}' >> all.log")
+    if spam_counter > 1:
         user = message.author
         muted = discord.utils.get(user.guild.roles, name="muted")
         verified = discord.utils.get(user.guild.roles, name="Member")
         await user.add_roles(muted)
         await user.remove_roles(verified)
-        system(f"echo 'USUARIO MUTEADO: {date} {mensaje}' >> muted.log")
+        system(f"echo 'Muted User: {date} {message_content}' >> muted.log")
     await bot.process_commands(message)
-bot.run('')
+bot.run(bot_config['token'])
