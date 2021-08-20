fc736f8f55032e0c2787e10458cf6e156562a3dc
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-08-20 20:06:47 -0300

    Now ban message has the user's tag

--- main/embed/ban.py
+++ main/embed/ban.py
@@ -3,6 +3,6 @@ from .embed import Embed
 class Ban(Embed):
 
   def __init__(self, user, message):
-    super().__init__(user, "", f"**Usuario Baneado**\n\nMotivo:\n```\n{message}```", None, None, 16122, None, None, None)
+    super().__init__("**Usuario Baneado**", "", f"<@{user.id}>\n\nMotivo:\n```\n{message}```", None, None, 16122, None, None, None)
 
   
